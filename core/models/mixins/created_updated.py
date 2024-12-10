f0533004c1b597ba2cc94c0212b7095d98e994ad
Danilov Aleksandr <aleksandr.danilov@auxo.team>
2024-12-10 17:45:22 +0300

    add new mixin

---
+++ core/models/mixins/created_updated.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import DateTime, func, text
+
+from datetime import datetime
+
+
+class CreatedUpdatedMixin:
+    created: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=text("TIMEZONE('utc', now())")
+    )
+    updated: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=text("TIMEZONE('utc', now())"), onupdate=datetime.now()
+    )
