c912128e17816a51537810c802f7b2d3f5e83e21
Danilov Aleksandr <aleksandr.danilov@auxo.team>
2024-12-10 17:45:50 +0300

    update task and category models

--- core/models/category.py
+++ core/models/category.py
@@ -2,7 +2,8 @@ from sqlalchemy.orm import Mapped
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
+from .mixins.created_updated import CreatedUpdatedMixin
 
 
-class Category(IntIdPkMixin, Base):
+class Category(IntIdPkMixin, CreatedUpdatedMixin, Base):
     name: Mapped[str]
--- core/models/task.py
+++ core/models/task.py
@@ -1,9 +1,12 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
+from .mixins.created_updated import CreatedUpdatedMixin
 
 
-class Task(IntIdPkMixin, Base):
+class Task(IntIdPkMixin, CreatedUpdatedMixin, Base):
     title: Mapped[str]
     pomodoro_count: Mapped[int]
+    category_id: Mapped[int] = mapped_column(ForeignKey("categorys.id", ondelete='CASCADE'))
