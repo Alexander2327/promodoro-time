919a69f86ee3e479b0efcd93cf10bd876cc37373
Danilov Aleksandr <aleksandr.danilov@auxo.team>
2024-12-11 13:57:12 +0300

    refactor deps

--- api/api_v1/categories.py
+++ api/api_v1/categories.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends
 from starlette import status
 
@@ -12,7 +14,7 @@ router = APIRouter(
 
 @router.get("/", response_model=list[CategoryRead], status_code=status.HTTP_200_OK)
 async def get_categories(
-        category_service: CategoryService = Depends(get_category_service),
+        category_service: Annotated[CategoryService, Depends(get_category_service)],
     ):
     return await category_service.get_categories()
 
@@ -20,7 +22,7 @@ async def get_categories(
 @router.get("/{category_id}", response_model=CategoryRead, status_code=status.HTTP_200_OK)
 async def get_category(
         category_id: int,
-        category_service: CategoryService = Depends(get_category_service),
+        category_service: Annotated[CategoryService, Depends(get_category_service)],
     ):
     return await category_service.get_category(category_id)
 
@@ -28,13 +30,14 @@ async def get_category(
 @router.post("/", response_model=CategoryRead, status_code=status.HTTP_201_CREATED)
 async def post_category(
         category_create: CategoryCreate,
-        category_service: CategoryService = Depends(get_category_service),):
+        category_service: Annotated[CategoryService, Depends(get_category_service)],
+    ):
    return await category_service.add_category(category_create)
 
 
 @router.delete("/{category_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_category(
         category_id: int,
-        category_service: CategoryService = Depends(get_category_service),
+        category_service: Annotated[CategoryService, Depends(get_category_service)],
     ):
     return await category_service.delete_category(category_id)
