01efb5116a45f28ec74951cddf06669b8b98feb5
Danilov Aleksandr <aleksandr.danilov@auxo.team>
2024-12-12 12:01:56 +0300

    create relationship between task and user

--- core/models/task.py
+++ core/models/task.py
@@ -1,13 +1,18 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
 from .mixins.created_updated import CreatedUpdatedMixin
 
+if TYPE_CHECKING:
+    from .user import User
 
 class Task(IntIdPkMixin, CreatedUpdatedMixin, Base):
     title: Mapped[str]
     pomodoro_count: Mapped[int]
     category_id: Mapped[int] = mapped_column(ForeignKey("categorys.id", ondelete='CASCADE'))
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete='CASCADE'))
+    user: Mapped["User"] = relationship(back_populates="tasks")
--- core/models/user.py
+++ core/models/user.py
@@ -1,10 +1,16 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.models import Base
 from core.models.mixins.created_updated import CreatedUpdatedMixin
 from core.models.mixins.int_id_pk import IntIdPkMixin
 
+if TYPE_CHECKING:
+    from .task import Task
+
 
 class User(IntIdPkMixin, CreatedUpdatedMixin, Base):
     username: Mapped[str] = mapped_column(String(30), unique=True)
+    tasks: Mapped[list["Task"]] = relationship(back_populates="user")
