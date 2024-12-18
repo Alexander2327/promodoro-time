7e7cc3e96dd75dd13006c1f26835bc4751b24296
Danilov Aleksandr <aleksandr.danilov@auxo.team>
2024-12-18 16:28:40 +0300

    add senrty integration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 from contextlib import asynccontextmanager
 
+import sentry_sdk
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -13,6 +14,18 @@ from exceptions import (BadRequestException, NotFoundException,
                         custom_exception_handler)
 from middleware import add_process_time_header
 
+sentry_sdk.init(
+    dsn="https://d7cd67a8f52b89d14aa28f2da0de9536@o4508489346121728.ingest.de.sentry.io/4508489350971472",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    _experiments={
+        # Set continuous_profiling_auto_start to True
+        # to automatically start the profiler on when
+        # possible.
+        "continuous_profiling_auto_start": True,
+    },
+)
 logging.basicConfig(
     # level=logging.INFO
     format=settings.logging.log_format,
